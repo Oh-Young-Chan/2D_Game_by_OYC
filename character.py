2aef82d1982934c04c71b044050411c04e0f0b03
Oh-Young-Chan <oyc0507@naver.com>
2019-10-27 22:44:30 +0900

    4주차 일반 적 오브젝트 구현
    2015180052 오영찬
    
    근거리 적, 원거리 적 구현입니다.

--- character.py
+++ character.py
@@ -2,32 +2,110 @@ from pico2d import *
 
 open_canvas()           # FRAME * 110, 1030, 160, 170, x, 120
 
-character = load_image('character_animation.png')
-ground_start = load_image('ground1x1.png')
-ground = load_image('ground1x2.png')
-flatform1of3 = load_image('flatform1x1.png')
-flatform2of3 = load_image('flatform1x2.png')
-flatform3of3 = load_image('flatform1x3.png')
-background = load_image('back.png')
-HP_bar = load_image('HP_bar.png')
-damaged_HP_bar = load_image('Damaged_HP_Bar_part.png')
-spike = load_image('spike.png')
-
 running = True
-damaging = False
-form = True
-flat = False
-HP = 100
-hit_count = 0
-x = 400
-y = 0
-f_y = 0
+i = 0
 dir = 0
 dir_y = 0
-i = 0
 vector = 6
-frame = 0
 adjust_on = False
+
+class Girl:
+    def __init__(self):
+        self.x, self.y = 400, 0
+        self.frame = 0
+        self.vector = 6
+        self.hit_count = 0
+        self.image = load_image('character_animation.png')
+
+    def update(self, dir, dir_y, vector):
+        self.frame = (self.frame+1) % 5
+        self.x += dir * 5
+        self.y = dir_y * 10
+        self.vector = vector
+
+    def draw(self):
+        self.image.clip_draw(self.frame * 96, self.vector * 96, 96, 96, self.x, 90 + self.y)
+
+    def damaged(self, damaged):
+        self.hit_count += 1
+        hp_bar.decrease_HP(damaged)
+        if (self.vector == 4):
+            self.vector = 0
+            self.image.clip_draw(self.frame * 96, self.vector * 96, 96, 96, self.x, 90 + self.y)
+        elif (self.vector == 5):
+            self.vector = 1
+            self.image.clip_draw(self.frame * 96, self.vector * 96, 96, 96, self.x, 90 + self.y)
+
+class Background:
+    def __init__(self):
+        self.x, self.y = 400, 300
+        self.image = load_image('back.png')
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
+
+class Ground:
+    def __init__(self):
+        self.x, self.y = 0, 30
+        self.image_start = load_image('ground1x1.png')
+        self.image_middle = load_image('ground1x2.png')
+
+    def draw(self):
+        self.image_start.draw(0, self.y)
+        for i in range(0, 11+1):
+            self.image_middle.draw(64 + 64 * i, self.y)
+
+class HP_BAR:
+    def __init__(self):
+        self.HP = 100
+        self.damaging = False
+        self.image = load_image('HP_Bar.png')
+        self.damaged_image = load_image('Damaged_HP_Bar_part.png')
+
+    def draw(self):
+        self.image.draw(150, 550)
+
+    def decrease_HP(self, damage):
+        self.HP -= damage
+        self.damaging = True
+
+class Spike:
+    def __init__(self):
+        self.x, self.y = 700, 70
+        self.image = load_image('spike.png')
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
+
+class Monster_bear:
+    def __init__(self):
+        self.x, self.y = 500, 120
+        self.frame = 0
+        self.image = load_image('Bear.png')
+
+    def update(self):
+        self.frame = (self.frame+1) % 4
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
+
+class Monster_mage:
+    def __init__(self):
+        self.x, self.y = 100, 95
+        self.frame = 0
+        self.a_frame = 0
+        self.image = load_image('Mage_Attack.png')
+        self.attack_image = load_image('Attack_ball.png')
+
+    def update(self):
+        self.frame = (self.frame+1) % 8
+        self.a_frame = (self.a_frame+1) % 5
+        delay(0.05)
+
+    def draw(self):
+        self.image.clip_draw(self.frame * 89, 0, 89, 89, self.x, self.y)
+        self.attack_image.clip_draw(self.a_frame * 16, 0, 16, 9, 180 + (self.a_frame*5), 90, 40, 40)
+
 
 def handle_events():
     global running
@@ -35,6 +113,7 @@ def handle_events():
     global dir_y
     global adjust_on
     global vector
+
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -75,33 +154,6 @@ def flatform():
             flat = False
             form = True
 
-def damaged(damaged):
-    global vector
-    global hit_count
-    hit_count += 1
-    decrease_HP(damaged)
-    if (vector == 4):
-        vector = 0
-        character.clip_draw(frame * 96, vector * 96, 96, 96, x, 90 + y)
-    elif (vector == 5):
-        vector = 1
-        character.clip_draw(frame * 96, vector * 96, 96, 96, x, 90 + y)
-
-def decrease_HP(damage):
-    global HP
-    global damaging
-    HP -= damage
-    damaging = True
-
-def black_HP():
-    if damaging:
-        for i in range(hit_count):
-            if i <= 21:
-                fill_black_HP(i)
-
-def fill_black_HP(i):
-    damaged_HP_bar.draw(263 - i * 11, 550)
-
 def adjust_jump():
     global dir_y
     global adjust_on
@@ -121,28 +173,44 @@ def work_adjust_jump():
     if dir_y == 0:
         adjust_on = True
 
-while running :
+def black_HP():
+    if hp_bar.damaging:
+        for i in range(girl.hit_count):
+            if i <= 21:
+                hp_bar.damaged_image.draw(263 - i * 11, 550)
+
+girl = Girl()
+background = Background()
+ground = Ground()
+hp_bar = HP_BAR()
+spike = Spike()
+bear = Monster_bear()
+mage = Monster_mage()
+
+while running:
     clear_canvas()
-    background.draw(400, 300)
-    ground_start.draw(0, 30)
-    flatform1of3.draw(200, 130)
-    flatform2of3.draw(232, 130)
-    flatform3of3.draw(264, 130)
-    flatform()
-    for i in range(15):
-        ground.draw(64 + i*64, 30)
-    HP_bar.draw(150, 550)
-    spike.draw(600, 70)
-    if (x > 580 and x < 620 and y <= 50):
-        damaged(50)
-    character.clip_draw(frame * 96, vector * 96, 96, 96, x, 90 + y + f_y)
+
+    background.draw()
+    ground.draw()
+    hp_bar.draw()
+    spike.draw()
+    if (girl.x > 680 and girl.x < 720 and girl.y <= 50):
+        girl.damaged(50)
+    if (girl.x > 200 and girl.x < 240 and girl.y <= 50):
+        girl.damaged(50)
+    if (girl.x > 460 and girl.x < 500 and girl.y <= 50):
+        girl.damaged(50)
     black_HP()
+    girl.draw()
+    girl.update(dir, dir_y, vector)
+    bear.draw()
+    bear.update()
+    mage.draw()
+    mage.update()
+
     update_canvas()
     handle_events()
     adjust_jump()
-    frame = (frame + 1) % 5
-    x += dir * 5
-    y = dir_y * 13
     delay(0.03)
 
 close_canvas()
