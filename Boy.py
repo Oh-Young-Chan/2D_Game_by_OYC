acc1bbb0947eb77291934f71bf7c9f3dc382cd72
Oh-Young-Chan <oyc0507@naver.com>
2019-11-26 21:27:28 +0900

    기초공사(1)
    2015180052 오영찬
    
    - 자잘한 오류발생으로 엎고 다시 만드는 중
    - 대쉬 삭제, 공격 삭제
    - 점프 구현

--- Boy.py
+++ Boy.py
@@ -18,16 +18,14 @@ FRAMES_PER_RUN = 8
 FRAMES_PER_ATTACK = 10
 
 
-RIGHT_DOWN, LEFT_DOWN, RIGHT_UP, LEFT_UP, PRESS_X, PRESS_ATTACK, TIME_OUT, DASH = range(8)
+RIGHT_DOWN, LEFT_DOWN, RIGHT_UP, LEFT_UP, PRESS_X = range(5)
 
 key_event_table = {
     (SDL_KEYDOWN, SDLK_RIGHT): RIGHT_DOWN,
     (SDL_KEYDOWN, SDLK_LEFT): LEFT_DOWN,
     (SDL_KEYUP, SDLK_RIGHT): RIGHT_UP,
     (SDL_KEYUP, SDLK_LEFT): LEFT_UP,
-    (SDL_KEYDOWN, SDLK_x): PRESS_X,
-    (SDL_KEYDOWN, SDLK_z): PRESS_ATTACK,
-    (SDL_KEYDOWN, SDLK_c): DASH
+    (SDL_KEYDOWN, SDLK_x): PRESS_X
 }
 
 
@@ -51,35 +49,27 @@ class IdleState:
 
     @staticmethod
     def exit(boy, event):
-        global dir_y
-
-        if event == DASH:
-            if boy.dir == 1:
-                boy.x += 30
-            else:
-                boy.x -= 30
-
-        boy.jump_timer = 10
-        dir_y = clamp(0, dir_y, 90)
+        pass
 
     @staticmethod
     def do(boy):
-        global dir_y
-
         boy.frame = (boy.frame + FRAMES_PER_IDLE * ACTION_PER_TIME * game_framework.frame_time * 0.2) % 13
-        boy.jump_timer -= 1
+        boy.x += boy.velocity * game_framework.frame_time
+        boy.x = clamp(25, boy.x, 800 - 25)
 
-        if boy.jump_timer <= 0:
-            dir_y -= 10
-            dir_y = clamp(0, dir_y, 90)
+        if boy.jump_timer_other > 0:                               # 공중에서 키보드 입력으로 Run 상태로 바뀌어도 상승과 낙하를 유지해준다.
+            boy.y += 10
+            boy.jump_timer_other -= 1
+        elif boy.jump_timer_other <= 0:
+            boy.y -= 10
+            boy.y = clamp(108, boy.y, 500)
 
     @staticmethod
     def draw(boy):
-        global dir_y
         if boy.dir == 1:
-            boy.image.clip_draw(int(boy.frame) * 32, 480, 32, 32, boy.x, 108 + boy.y + dir_y, 100, 100)
+            boy.image.clip_draw(int(boy.frame) * 32, 480, 32, 32, boy.x, boy.y, 100, 100)
         else:
-            boy.image.clip_draw(int(boy.frame) * 32, 224, 32, 32, boy.x, 108 + boy.y + dir_y, 100, 100)
+            boy.image.clip_draw(int(boy.frame) * 32, 224, 32, 32, boy.x, boy.y, 100, 100)
 
 
 class RunState:
@@ -100,39 +90,28 @@ class RunState:
 
     @staticmethod
     def exit(boy, event):
-        global dir_y
-
-        if event == DASH:
-            if boy.dir == 1:
-                boy.x += 30
-            else:
-                boy.x -= 30
-
-        boy.jump_timer = 10
-        dir_y = clamp(0, dir_y, 90)
+        pass
 
     @staticmethod
     def do(boy):
-        global dir_y
-
         boy.frame = (boy.frame + FRAMES_PER_RUN * ACTION_PER_TIME * game_framework.frame_time) % 8
         boy.x += boy.velocity * game_framework.frame_time
         boy.x = clamp(25, boy.x, 800 - 25)
 
-        boy.jump_timer -= 1
+        if boy.jump_timer_other > 0:                     # 공중에서 키보드 입력으로 Run 상태로 바뀌어도 상승과 낙하를 유지해준다.
+            boy.y += 10
+            boy.jump_timer_other -= 1
+        elif boy.jump_timer_other <= 0:
+            boy.y -= 10
+            boy.y = clamp(108, boy.y, 500)
 
-        if boy.jump_timer <= 0:
-            dir_y -= 10
-            dir_y = clamp(0, dir_y, 90)
 
     @staticmethod
     def draw(boy):
-        global dir_y
-
         if boy.dir == 1:
-            boy.image.clip_draw(int(boy.frame) * 32, 448, 32, 32, boy.x, 108 + boy.y + dir_y, 100, 100)
+            boy.image.clip_draw(int(boy.frame) * 32, 448, 32, 32, boy.x, boy.y, 100, 100)
         else:
-            boy.image.clip_draw(int(boy.frame) * 32, 192, 32, 32, boy.x, 108 + boy.y + dir_y, 100, 100)
+            boy.image.clip_draw(int(boy.frame) * 32, 192, 32, 32, boy.x, boy.y, 100, 100)
 
 
 class JumpState:
@@ -153,57 +132,36 @@ class JumpState:
 
     @staticmethod
     def exit(boy, event):
-        global dir_y
+        boy.jump_timer_other = boy.jump_timer               # 공중에서 키보드 입력으로 Idle 상태로 바뀌어도 상승과 낙하를 유지해준다.
 
-        if event == DASH:
-            if boy.dir == 1:
-                boy.x += 30
-            else:
-                boy.x -= 30
-
-        boy.jump_timer = 10
-        dir_y = clamp(0, dir_y, 90)
+        boy.jump_timer = 20
 
     @staticmethod
     def do(boy):
-        global dir_y
-
         boy.frame = (boy.frame + FRAMES_PER_RUN * ACTION_PER_TIME * game_framework.frame_time) % 8
         boy.x += boy.velocity * game_framework.frame_time
         boy.x = clamp(25, boy.x, 800 - 25)
-        dir_y = clamp(0, dir_y, 90)
 
         if boy.jump_timer > 0:
-            dir_y += 10
+            boy.y += 10
             boy.jump_timer -= 1
         elif boy.jump_timer <= 0:
-            dir_y -= 10
-            dir_y = clamp(0, dir_y, 90)
+            boy.y -= 10
+            boy.y = clamp(108, boy.y, 500)
 
 
     @staticmethod
     def draw(boy):
-        global dir_y
         if boy.dir == 1:
-            boy.image.clip_draw(int(boy.frame) * 32, 448, 32, 32, boy.x, 108 + boy.y + dir_y, 100, 100)
+            boy.image.clip_draw(int(boy.frame) * 32, 448, 32, 32, boy.x, boy.y, 100, 100)
         else:
-            boy.image.clip_draw(int(boy.frame) * 32, 192, 32, 32, boy.x, 108 + boy.y + dir_y, 100, 100)
+            boy.image.clip_draw(int(boy.frame) * 32, 192, 32, 32, boy.x, boy.y, 100, 100)
 
 
 class AttackState:
     @staticmethod
     def enter(boy, event):
-        if event == RIGHT_DOWN:
-            boy.dir = 1
-        elif event == LEFT_DOWN:
-            boy.dir = 0
-        elif event == RIGHT_UP:
-            boy.dir = 1
-        elif event == LEFT_UP:
-            boy.dir = 0
-
         boy.attack_count += 1
-        print(boy.attack_count)
 
     @staticmethod
     def exit(boy, event):
@@ -212,8 +170,6 @@ class AttackState:
     @staticmethod
     def do(boy):
         boy.frame = (boy.frame + FRAMES_PER_ATTACK * ACTION_PER_TIME * game_framework.frame_time) % 10
-        boy.x += boy.velocity * game_framework.frame_time
-        boy.x = clamp(25, boy.x, 800 - 25)
 
         if boy.frame > 9:
             boy.add_event(TIME_OUT)
@@ -224,47 +180,42 @@ class AttackState:
 
         if boy.attack_count == 1:
             if boy.dir == 1:
-                boy.image.clip_draw(int(boy.frame) * 32, 416, 32, 32, boy.x, 108 + boy.y + dir_y, 100, 100)
+                boy.image.clip_draw(int(boy.frame) * 32, 416, 32, 32, boy.x, boy.y, 100, 100)
             else:
-                boy.image.clip_draw(int(boy.frame) * 32, 160, 32, 32, boy.x, 108 + boy.y + dir_y, 100, 100)
+                boy.image.clip_draw(int(boy.frame) * 32, 160, 32, 32, boy.x, boy.y, 100, 100)
         elif boy.attack_count == 2:
             if boy.dir == 1:
-                boy.image.clip_draw(int(boy.frame) * 32, 384, 32, 32, boy.x, 108 + boy.y + dir_y, 100, 100)
+                boy.image.clip_draw(int(boy.frame) * 32, 384, 32, 32, boy.x, boy.y, 100, 100)
             else:
-                boy.image.clip_draw(int(boy.frame) * 32, 128, 32, 32, boy.x, 108 + boy.y + dir_y, 100, 100)
-        elif boy.attack_count == 3:
+                boy.image.clip_draw(int(boy.frame) * 32, 128, 32, 32, boy.x, boy.y, 100, 100)
+        else:
             if boy.dir == 1:
-                boy.image.clip_draw(int(boy.frame) * 32, 352, 32, 32, boy.x, 108 + boy.y + dir_y, 100, 100)
+                boy.image.clip_draw(int(boy.frame) * 32, 352, 32, 32, boy.x, boy.y, 100, 100)
             else:
-                boy.image.clip_draw(int(boy.frame) * 32, 96, 32, 32, boy.x, 108 + boy.y + dir_y, 100, 100)
+                boy.image.clip_draw(int(boy.frame) * 32, 96, 32, 32, boy.x, boy.y, 100, 100)
 
 
 
 next_state_table = {
     IdleState: {RIGHT_UP: RunState, LEFT_UP: RunState,
                 RIGHT_DOWN: RunState, LEFT_DOWN: RunState,
-                PRESS_X: JumpState, PRESS_ATTACK: AttackState,
-                DASH: IdleState},
+                PRESS_X: JumpState},                  # TIME_OUT: IdleState 추가해야 공격할 때 다른 버튼 연타해도 안 튕김
     RunState: {RIGHT_UP: IdleState, LEFT_UP: IdleState,
-               LEFT_DOWN: IdleState, RIGHT_DOWN: IdleState,
-               PRESS_X: JumpState, PRESS_ATTACK: AttackState,
-               DASH: RunState},
-    JumpState: {RIGHT_UP: IdleState, LEFT_UP: IdleState,
+               LEFT_DOWN: RunState, RIGHT_DOWN: RunState,
+               PRESS_X: JumpState},
+    JumpState: {RIGHT_UP: RunState, LEFT_UP: RunState,
                 LEFT_DOWN: RunState, RIGHT_DOWN: RunState,
-                PRESS_X: JumpState, DASH: JumpState},
-    AttackState: {RIGHT_UP: IdleState, LEFT_UP: IdleState,
-                  LEFT_DOWN: RunState, RIGHT_DOWN: RunState,
-                  PRESS_X: JumpState, TIME_OUT: IdleState,
-                  PRESS_ATTACK: AttackState}
+                PRESS_X: JumpState}
 }
 
 
 class Boy:
     def __init__(self):
-        self.x, self.y = 400, 0
+        self.x, self.y = 400, 108
         self.image = load_image('Adventurer Sprite Sheet v1.1.png')
         self.dir = 1
-        self.jump_timer = 10
+        self.jump_timer = 20
+        self.jump_timer_other = 0
         self.velocity = 0
         self.frame = 0
         self.HP = 100
@@ -272,6 +223,9 @@ class Boy:
         self.event_que = []
         self.cur_state = IdleState
         self.cur_state.enter(self, None)
+
+    def get_bb(self):
+        return self.x - 50, self.y - 50, self.x + 50, self.y + 50
 
     def update_state(self):
         if len(self.event_que) > 0:
@@ -293,10 +247,12 @@ class Boy:
             self.cur_state = next_state_table[self.cur_state][event]
             self.cur_state.enter(self, event)
 
-        print(dir_y)
+        print(self.cur_state)
+
 
     def draw(self):
         self.cur_state.draw(self)
+        draw_rectangle(*self.get_bb())
 
     def handle_event(self, event):
         if (event.type, event.key) in key_event_table:
